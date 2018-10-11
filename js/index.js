4ce2015cd4ef1764968c9b027efde8ef0548ae15
Brendan Early <mymindstorm1@gmail.com>
2018-10-11 17:53:35 -0500

    Section description hidden in popover

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$.ajax({method: 'GET', url: '../data/data.json'}).done(data => {
+$.ajax({ method: 'GET', url: '../data/data.json' }).done(data => {
     $(document).ready(function () {
         $('#header').text(data.name);
         $('#subheader').html(data.data.description);
@@ -7,7 +7,7 @@ $.ajax({method: 'GET', url: '../data/data.json'}).done(data => {
         let iconprototype = ""
         $.each(data.children, (index, category) => {
             cardprototype = [];
-            cardprototype.push(...['<div class="col-sm pb-4">', '<div class="card">', '<div class="card-header">', '<p class="card-title">',category.name, '</p>', '<p class="card-subtitle mb-2 text-muted">', category.data.description, '</p>', '</div>', '<div class="card-block">', '<ul class="list-group list-group-flush">']);
+            cardprototype.push(...['<div class="col-sm pb-4">', '<div class="card" id="' + String(String(category.name).toLowerCase()).replace(" ", "") + '">', '<div class="card-header">', '<div class="card-title" data-toggle="popover" data-content="' + category.data.description + '">', category.name, '</div>', '</div>', '<div class="card-block">', '<ul class="list-group list-group-flush">']);
             $.each(category.children, (index, item) => {
                 if (item.data.icon) {
                     iconprototype = '<div class="icon"><span class="helper"><img src="' + '/img/icons/' + item.data.icon + '" height="42" width="42"></span></div>'
@@ -20,5 +20,6 @@ $.ajax({method: 'GET', url: '../data/data.json'}).done(data => {
             bodyprototype.push(...cardprototype);
         });
         $('.bodycontent').html(bodyprototype.join(""));
+        $('[data-toggle="popover"]').popover({ trigger: 'hover' })
     });
 });
